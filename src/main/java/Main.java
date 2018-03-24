885e254d482c23e33deb5999170649b92a7d74ec
sawluk.michal <sawluk.michal@gmail.com>
2018-03-24 20:12:54 +0100

    Zmiana nazwy projektu

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,7 +9,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("Logz.fxml"));
-        primaryStage.setTitle("LogzUnpacker");
+        primaryStage.setTitle("UnPacker");
         primaryStage.setScene(new Scene(root, 600, 440));
         primaryStage.show();
 
